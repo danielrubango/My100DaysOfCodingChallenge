c693e0431fbb0424f841a3b91ac08422a17623ca
Daniel RUBANGO <danielrubango@gmail.com>
2018-06-08 16:14:31 +0100

    Converting identation to space

--- Round1_Day1/FizzBuzz.js
+++ Round1_Day1/FizzBuzz.js
@@ -1,29 +1,29 @@
 // Eloquent JavaScript : Exercice 2
 // FizzBuzz Coding interview
 /*
-	Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
-	For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3),
-	print "Buzz" instead.
+    Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
+    For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3),
+    print "Buzz" instead.
 
-	When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible
-	by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+    When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible
+    by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
  */
 
 for(let n = 1; n <= 100; n++) {
-	let msg = '';
+    let msg = '';
 
-	if (n % 3 === 0) {
-		msg += 'Fizz';
-	}
+    if (n % 3 === 0) {
+        msg += 'Fizz';
+    }
 
-	if (n % 5 === 0) {
-		msg += 'Buzz';
-	}
+    if (n % 5 === 0) {
+        msg += 'Buzz';
+    }
 
-	if (n % 3 !== 0 && n % 5 !== 0) {
-		console.log(n);
-		continue;
-	}
+    if (n % 3 !== 0 && n % 5 !== 0) {
+        console.log(n);
+        continue;
+    }
 
-	console.log(msg);
+    console.log(msg);
 }
--- Round1_Day1/looping_triangle.js
+++ Round1_Day1/looping_triangle.js
@@ -1,17 +1,17 @@
 // Eloquent JavaScript : Exercice 1
 // Looping triangle
 /*
-	Write a loop that makes seven calls to console.log to output the following triangle:
+    Write a loop that makes seven calls to console.log to output the following triangle:
 
-		#
-		##
-		###
-		####
-		#####
-		######
-		#######
+        #
+        ##
+        ###
+        ####
+        #####
+        ######
+        #######
 */
 
 for(let log = '#'; log.length <= 7; log += '#') {
-	console.log(log);
+    console.log(log);
 }
